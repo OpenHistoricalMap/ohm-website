823c2e7b30748e529607194253da003a04dadf23
Rub21 <rub2106@gmail.com>
2025-10-01 18:09:03 -0500

    Fix map issues in non-English locales

--- app/assets/javascripts/leaflet.ohm.js
+++ app/assets/javascripts/leaflet.ohm.js
@@ -1,7 +1,11 @@
 L.OSM.OHM = L.OSM.MaplibreGL.extend({
   onAdd: function (map) {
+    const styleName = (this.options.layerId || this.options.name?.replaceAll(' ', ''))
+      .trim()
+      .replace(/\w\S*/g, w => w[0].toUpperCase() + w.slice(1).toLowerCase());
+
     L.OSM.MaplibreGL.prototype.onAdd.call(this, map);
-    this.getMaplibreMap().setStyle(ohmVectorStyles[this.options.name.replaceAll(' ','')]);
+    this.getMaplibreMap().setStyle(ohmVectorStyles[styleName]);
   },
   onRemove: function (map) {
     L.OSM.MaplibreGL.prototype.onRemove.call(this, map);
