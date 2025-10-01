bbf4b950ad75ce45cdfae9896a72286b7309030e
Rub21 <rub2106@gmail.com>
2025-10-01 18:02:59 -0500

    Disable setRTLTextPlugin in leaflet.maplibre.js

--- app/assets/javascripts/leaflet.maplibre.js
+++ app/assets/javascripts/leaflet.maplibre.js
@@ -1,7 +1,7 @@
 //= require maplibre-gl
 //= require @maplibre/maplibre-gl-leaflet
 
-maplibregl.setRTLTextPlugin(OSM.RTL_TEXT_PLUGIN, true);
+// maplibregl.setRTLTextPlugin(OSM.RTL_TEXT_PLUGIN, true);
 
 L.OSM.MaplibreGL = L.MaplibreGL.extend({
   getAttribution: function () {
