f7c29d95da08ad03ad0c8a9d033d02c20424be62
Minh Nguyễn <mxn@1ec5.org>
2025-10-23 12:14:31 -0700

    Hard-code package style names as part of constructors

--- app/assets/javascripts/leaflet.ohm.js
+++ app/assets/javascripts/leaflet.ohm.js
@@ -1,18 +1,14 @@
 L.OSM.OHM = L.OSM.MaplibreGL.extend({
   onAdd: function (map) {
-    const styleName = (this.options.layerId || this.options.name?.replaceAll(' ', ''))
-      .trim()
-      .replace(/\w\S*/g, w => w[0].toUpperCase() + w.slice(1).toLowerCase());
-
     L.OSM.MaplibreGL.prototype.onAdd.call(this, map);
-    this.getMaplibreMap().setStyle(ohmVectorStyles[styleName]);
+    this.getMaplibreMap().setStyle(ohmVectorStyles[this.ohmStyleName]);
   },
   onRemove: function (map) {
     L.OSM.MaplibreGL.prototype.onRemove.call(this, map);
   }
 });
 
-L.OSM.Historical = L.OSM.OHM.extend({});
-L.OSM.Railway = L.OSM.OHM.extend({});
-L.OSM.Woodblock = L.OSM.OHM.extend({});
-L.OSM.JapaneseScroll = L.OSM.OHM.extend({});
+L.OSM.Historical = L.OSM.OHM.extend({ ohmStyleName: "Historical" });
+L.OSM.Railway = L.OSM.OHM.extend({ ohmStyleName: "Railway"});
+L.OSM.Woodblock = L.OSM.OHM.extend({ ohmStyleName: "Woodblock" });
+L.OSM.JapaneseScroll = L.OSM.OHM.extend({ ohmStyleName: "JapaneseScroll" });
