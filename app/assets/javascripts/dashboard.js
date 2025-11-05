3692342ee53a8fcd9a2d2c8fbfe1954271f86692
Frank Elsinga <frank@elsinga.de>
2025-11-05 23:29:07 +0100

    Migrate the dashboard entry page from leaflet to maplibre

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -1,32 +1,52 @@
-//= require leaflet.locate
-
 $(function () {
   const defaultHomeZoom = 12;
   let map;
 
   if ($("#map").length) {
-    map = L.map("map", {
+    const position = $("html").attr("dir") === "rtl" ? "top-left" : "top-right";
+
+    map = new maplibregl.Map({
+      container: "map",
+      style: {
+        "version": 8,
+        "name": "OSM Raster",
+        "sources": {
+          "osm": {
+            "type": "raster",
+            "tiles": [
+              "https://tile.openstreetmap.org/{z}/{x}/{y}.png"
+            ],
+            "tileSize": 256,
+            "maxzoom": 19,
+            "attribution": "© OpenStreetMap contributors"
+          }
+        },
+        "layers": [
+          {
+            "id": "osm",
+            "type": "raster",
+            "source": "osm",
+          }
+        ],
+        "projection": { "type": "globe" },
+      },
       attributionControl: false,
-      zoomControl: false
-    }).addLayer(new L.OSM.Mapnik());
+      center: OSM.home ? [OSM.home.lon, OSM.home.lat] : [0, 0],
+      zoom: OSM.home ? defaultHomeZoom : 0
+    });
 
-    const position = $("html").attr("dir") === "rtl" ? "topleft" : "topright";
-
-    L.OSM.zoom({ position }).addTo(map);
-
-    L.OSM.locate({ position }).addTo(map);
-
-    if (OSM.home) {
-      map.setView([OSM.home.lat, OSM.home.lon], defaultHomeZoom);
-    } else {
-      map.setView([0, 0], 0);
-    }
+    map.addControl(new maplibregl.NavigationControl(), position);
 
     $("[data-user]").each(function () {
-      const user = $(this).data("user");
-      if (user.lon && user.lat) {
-        L.marker([user.lat, user.lon], { icon: OSM.getMarker({ color: user.color }) }).addTo(map)
-          .bindPopup(user.description, { minWidth: 200 });
+      const userData = $(this).data("user");
+      if (userData && userData.lon && userData.lat) {
+        const popup = new maplibregl.Popup()
+          .setHTML(userData.description);
+
+        new maplibregl.Marker({ color: userData.color })
+          .setLngLat([userData.lon, userData.lat])
+          .setPopup(popup)
+          .addTo(map);
       }
     });
   }
