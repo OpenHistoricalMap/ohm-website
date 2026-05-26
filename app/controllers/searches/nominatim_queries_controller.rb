53942ac7f940bbd68be221ffb37ca7ec40c2933f
Eric Theise <erictheise@gmail.com>
2026-05-26 00:00:55 -0700

    Don't loop if place.elements[extratags] is nil

--- app/controllers/searches/nominatim_queries_controller.rb
+++ app/controllers/searches/nominatim_queries_controller.rb
@@ -12,7 +12,6 @@ module Searches
 
       # extract the results from the response
       results = response.elements["searchresults"]
-
       # create result array
       @results = []
 
@@ -66,7 +65,7 @@ module Searches
           elsif extratag.attributes["key"] == "end_date"
             end_date = extratag.attributes['value']
           end
-        end
+        end unless place.elements["extratags"].nil?
         if start_date || end_date
           suffix = t "geocoder.search_osm_nominatim.suffix_format", :dates => DateRange.new(start_date, end_date).to_s
         end
